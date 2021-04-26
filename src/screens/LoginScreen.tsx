ca7358ceac0b414b2f95313af5954b8f592e8962
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-26 08:57:44 -0300

    Login screen advanced styles with correct position and button

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {Background} from '../components/Background';
 import {WhiteLogo} from '../components/WhiteLogo';
 import {loginStyles} from '../theme/loginTheme';
@@ -8,17 +15,48 @@ export const LoginScreen = () => {
   return (
     <>
       <Background />
+      <View style={loginStyles.formContainer}>
+        <WhiteLogo />
 
-      <WhiteLogo />
+        <Text style={loginStyles.title}>Login</Text>
+        <Text style={loginStyles.label}>Email</Text>
+        <TextInput
+          style={[
+            loginStyles.textInput,
+            Platform.OS === 'ios' && loginStyles.textInputIOS,
+          ]}
+          placeholder="Ingrese su email"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          keyboardType="email-address"
+          underlineColorAndroid="white"
+          selectionColor="white"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={loginStyles.label}>Password</Text>
+        <TextInput
+          style={[
+            loginStyles.textInput,
+            Platform.OS === 'ios' && loginStyles.textInputIOS,
+          ]}
+          placeholder="******"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          underlineColorAndroid="white"
+          selectionColor="white"
+          secureTextEntry={true}
+        />
+        <View style={loginStyles.buttonContainer}>
+          <TouchableOpacity onPress={() => {}} style={loginStyles.button}>
+            <Text style={loginStyles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
 
-      <Text style={loginStyles.title}>Login</Text>
-      <Text style={loginStyles.label}>Email</Text>
-      <TextInput
-        style={loginStyles.textInput}
-        placeholder="Ingrese su email"
-        placeholderTextColor="rgba(255,255,255,0.4)"
-        keyboardType="email-address"
-      />
+        <View style={loginStyles.newUserContainer}>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={loginStyles.buttonText}>Crear una nueva cuenta</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
