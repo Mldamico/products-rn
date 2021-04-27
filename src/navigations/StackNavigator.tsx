32cb3d1352fdb3af514b8386ae48dd03bd0e21fb
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-27 19:15:01 -0300

    Loading screen

--- src/navigations/StackNavigator.tsx
+++ src/navigations/StackNavigator.tsx
@@ -6,11 +6,17 @@ import {RegisterScreen} from '../screens/RegisterScreen';
 import {ProtectedScreen} from '../screens/ProtectedScreen';
 import {useContext} from 'react';
 import {AuthContext} from '../context/AuthContext';
+import {LoadingScreen} from '../screens/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 export function StackNavigator() {
   const {status} = useContext(AuthContext);
+
+  if (status === 'checking') {
+    return <LoadingScreen />;
+  }
+
   return (
     <Stack.Navigator
       screenOptions={{
